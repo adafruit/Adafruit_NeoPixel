0a65646b1ac12bfb63ec2b39560f1ccb2524b11d
Justin R. Cutler <justin.r.cutler@gmail.com>
2013-07-18 23:21:50 -0400

    Make "get" methods const

--- Adafruit_NeoPixel.h
+++ Adafruit_NeoPixel.h
@@ -52,7 +52,7 @@ class Adafruit_NeoPixel {
   static uint32_t
     Color(uint8_t r, uint8_t g, uint8_t b);
   uint32_t
-    getPixelColor(uint16_t n);
+    getPixelColor(uint16_t n) const;
 
  private:
 
