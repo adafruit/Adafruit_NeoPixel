0b43f1a31e0761c88aa56dce1c0c2a856b629183
Tony DiCola <tony@tonydicola.com>
2014-09-19 15:47:23 -0700

    Fix #23 by updating Wheel function to go R->G->B instead of G->R->B.

--- examples/strandtest/strandtest.ino
+++ examples/strandtest/strandtest.ino
@@ -109,14 +109,15 @@ void theaterChaseRainbow(uint8_t wait) {
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
   if(WheelPos < 85) {
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else if(WheelPos < 170) {
+    WheelPos -= 85;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   } else {
    WheelPos -= 170;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
   }
 }
 
