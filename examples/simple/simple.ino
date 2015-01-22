f77c55b45776b2c61f61131da7b4f3419e6ca8cc
ladyada <limor@ladyada.net>
2015-01-22 13:53:58 -0500

    add trinket 5V @ 16Mhz explicitly

--- examples/simple/simple.ino
+++ examples/simple/simple.ino
@@ -1,8 +1,11 @@
 // NeoPixel Ring simple sketch (c) 2013 Shae Erisson
 // released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
+
 #include <Adafruit_NeoPixel.h>
+#include <avr/power.h>
 
 // Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1
 #define PIN            6
 
 // How many NeoPixels are attached to the Arduino?
@@ -16,15 +19,27 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ80
 int delayval = 500; // delay for half a second
 
 void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+  // End of trinket special code
+
   pixels.begin(); // This initializes the NeoPixel library.
 }
 
 void loop() {
+
   // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+
   for(int i=0;i<NUMPIXELS;i++){
+
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
     pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+
     pixels.show(); // This sends the updated pixel color to the hardware.
+
     delay(delayval); // Delay for a period of time (in milliseconds).
+
   }
 }
--- examples/strandtest/strandtest.ino
+++ examples/strandtest/strandtest.ino
@@ -1,4 +1,5 @@
 #include <Adafruit_NeoPixel.h>
+#include <avr/power.h>
 
 #define PIN 6
 
@@ -17,6 +18,13 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
 // on a live circuit...if you must, connect GND first.
 
 void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+  // End of trinket special code
+
+
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
 }
