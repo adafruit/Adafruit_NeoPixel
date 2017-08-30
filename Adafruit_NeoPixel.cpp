c00f8d08285d2804f8b26808d2b1be6f48abcbc4
PaulStoffregen <paul@pjrc.com>
2017-08-30 06:59:27 -0700

    Improve check for Teensy-specific code

--- Adafruit_NeoPixel.cpp
+++ Adafruit_NeoPixel.cpp
@@ -1058,7 +1058,7 @@ void Adafruit_NeoPixel::show(void) {
 
 // ARM MCUs -- Teensy 3.0, 3.1, LC, Arduino Due ---------------------------
 
-#if defined(KINETISK) // Teensy 3.0, 3.1, 3.2, 3.5, 3.6
+#if defined(TEENSYDUINO) && defined(KINETISK) // Teensy 3.0, 3.1, 3.2, 3.5, 3.6
 #define CYCLES_800_T0H  (F_CPU / 4000000)
 #define CYCLES_800_T1H  (F_CPU / 1250000)
 #define CYCLES_800      (F_CPU /  800000)
@@ -1115,7 +1115,7 @@ void Adafruit_NeoPixel::show(void) {
   }
 #endif // NEO_KHZ400
 
-#elif defined(__MKL26Z64__) // Teensy-LC
+#elif defined(TEENSYDUINO) && defined(__MKL26Z64__) // Teensy-LC
 
 #if F_CPU == 48000000
   uint8_t          *p   = pixels,
