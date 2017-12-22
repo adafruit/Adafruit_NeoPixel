dcdcb5238bf8b731c5474e81cec078b5caf41d88
shop <shop@Rock-the-Bike.local>
2017-12-21 23:47:53 -0800

    make default number of repeats = 1

--- Adafruit_NeoPixel.h
+++ Adafruit_NeoPixel.h
@@ -118,7 +118,7 @@ class Adafruit_NeoPixel {
  public:
 
   // Constructor: number of LEDs, pin number, LED type
-  Adafruit_NeoPixel(uint16_t n, uint8_t p=6, neoPixelType t=NEO_GRB + NEO_KHZ800, uint8_t r=0);
+  Adafruit_NeoPixel(uint16_t n, uint8_t p=6, neoPixelType t=NEO_GRB + NEO_KHZ800, uint8_t r=1);
   Adafruit_NeoPixel(void);
   ~Adafruit_NeoPixel();
 
