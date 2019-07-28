74b2e76e5553a04e8b95d47330b857548919bdbf
mikeshub <mikesbaker@gmail.com>
2019-07-28 13:31:19 -0500

    Update esp pins for consistency in the code

--- Adafruit_NeoPixel.cpp
+++ Adafruit_NeoPixel.cpp
@@ -171,10 +171,10 @@ void Adafruit_NeoPixel::updateType(neoPixelType t) {
 #if defined(ESP8266)
 // ESP8266 show() is external to enforce ICACHE_RAM_ATTR execution
 extern "C" void ICACHE_RAM_ATTR espShow(
-  uint8_t pin, uint8_t *pixels, uint32_t numBytes, uint8_t type);
+  uint16_t pin, uint8_t *pixels, uint32_t numBytes, uint8_t type);
 #elif defined(ESP32)
 extern "C" void espShow(
-  uint8_t pin, uint8_t *pixels, uint32_t numBytes, uint8_t type);
+  uint16_t pin, uint8_t *pixels, uint32_t numBytes, uint8_t type);
 #endif // ESP8266
 
 /*!
