65f27d9b058063b10048c04c73ac1783005932ef
Travis CI <travis@travis-ci.org>
2019-07-30 00:54:05 +0000

    Deploy code docs to GitHub Pages Travis build: 213
    Commit: 5239de892cb671de841258b7cb3bb84559cd3286

--- html/search/all_a.js
+++ html/search/all_a.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['setbrightness',['setBrightness',['../class_adafruit___neo_pixel.html#aa05f7d37828c43fef00e6242dcf75959',1,'Adafruit_NeoPixel']]],
-  ['setpin',['setPin',['../class_adafruit___neo_pixel.html#a64a6704fdac239046791e7a9f7a76ab6',1,'Adafruit_NeoPixel']]],
+  ['setpin',['setPin',['../class_adafruit___neo_pixel.html#acaf844b8acd7246b13a71e1185675386',1,'Adafruit_NeoPixel']]],
   ['setpixelcolor',['setPixelColor',['../class_adafruit___neo_pixel.html#ab8763ccc6f9a090df1f753905fd5561e',1,'Adafruit_NeoPixel::setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b)'],['../class_adafruit___neo_pixel.html#ac7b3529b3d190f3af5d8e5b9ead27688',1,'Adafruit_NeoPixel::setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b, uint8_t w)'],['../class_adafruit___neo_pixel.html#a19fc274330c0e65907929ee03b93b1c3',1,'Adafruit_NeoPixel::setPixelColor(uint16_t n, uint32_t c)']]],
   ['show',['show',['../class_adafruit___neo_pixel.html#a4bcda7a15591065570d3598cbf970be7',1,'Adafruit_NeoPixel']]],
   ['sine8',['sine8',['../class_adafruit___neo_pixel.html#afb00024000da81bcb6378c7052f77be3',1,'Adafruit_NeoPixel']]]
