975fcf6d6fafbc7cf870b9cefd72e3bb08576c2d
dherrada <dylan.herrada@gmail.com>
2020-06-15 11:50:38 -0400

    Moved to actions, no doxygen necessary

--- esp8266.c
+++ esp8266.c
@@ -12,75 +12,80 @@
 static uint32_t _getCycleCount(void) __attribute__((always_inline));
 static inline uint32_t _getCycleCount(void) {
   uint32_t ccount;
-  __asm__ __volatile__("rsr %0,ccount":"=a" (ccount));
+  __asm__ __volatile__("rsr %0,ccount" : "=a"(ccount));
   return ccount;
 }
 
 #ifdef ESP8266
-void ICACHE_RAM_ATTR espShow(
- uint8_t pin, uint8_t *pixels, uint32_t numBytes, boolean is800KHz) {
+void ICACHE_RAM_ATTR espShow(uint8_t pin, uint8_t *pixels, uint32_t numBytes,
+                             boolean is800KHz) {
 #else
-void espShow(
- uint8_t pin, uint8_t *pixels, uint32_t numBytes, boolean is800KHz) {
+void espShow(uint8_t pin, uint8_t *pixels, uint32_t numBytes,
+             boolean is800KHz) {
 #endif
 
-#define CYCLES_800_T0H  (F_CPU / 2500000) // 0.4us
-#define CYCLES_800_T1H  (F_CPU / 1250000) // 0.8us
-#define CYCLES_800      (F_CPU /  800000) // 1.25us per bit
-#define CYCLES_400_T0H  (F_CPU / 2000000) // 0.5uS
-#define CYCLES_400_T1H  (F_CPU /  833333) // 1.2us
-#define CYCLES_400      (F_CPU /  400000) // 2.5us per bit
+#define CYCLES_800_T0H (F_CPU / 2500000) // 0.4us
+#define CYCLES_800_T1H (F_CPU / 1250000) // 0.8us
+#define CYCLES_800 (F_CPU / 800000)      // 1.25us per bit
+#define CYCLES_400_T0H (F_CPU / 2000000) // 0.5uS
+#define CYCLES_400_T1H (F_CPU / 833333)  // 1.2us
+#define CYCLES_400 (F_CPU / 400000)      // 2.5us per bit
 
   uint8_t *p, *end, pix, mask;
   uint32_t t, time0, time1, period, c, startTime;
 
 #ifdef ESP8266
   uint32_t pinMask;
-  pinMask   = _BV(pin);
+  pinMask = _BV(pin);
 #endif
 
-  p         =  pixels;
-  end       =  p + numBytes;
-  pix       = *p++;
-  mask      = 0x80;
+  p = pixels;
+  end = p + numBytes;
+  pix = *p++;
+  mask = 0x80;
   startTime = 0;
 
 #ifdef NEO_KHZ400
-  if(is800KHz) {
+  if (is800KHz) {
 #endif
-    time0  = CYCLES_800_T0H;
-    time1  = CYCLES_800_T1H;
+    time0 = CYCLES_800_T0H;
+    time1 = CYCLES_800_T1H;
     period = CYCLES_800;
 #ifdef NEO_KHZ400
   } else { // 400 KHz bitstream
-    time0  = CYCLES_400_T0H;
-    time1  = CYCLES_400_T1H;
+    time0 = CYCLES_400_T0H;
+    time1 = CYCLES_400_T1H;
     period = CYCLES_400;
   }
 #endif
 
-  for(t = time0;; t = time0) {
-    if(pix & mask) t = time1;                             // Bit high duration
-    while(((c = _getCycleCount()) - startTime) < period); // Wait for bit start
+  for (t = time0;; t = time0) {
+    if (pix & mask)
+      t = time1; // Bit high duration
+    while (((c = _getCycleCount()) - startTime) < period)
+      ; // Wait for bit start
 #ifdef ESP8266
-    GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, pinMask);       // Set high
+    GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, pinMask); // Set high
 #else
     gpio_set_level(pin, HIGH);
 #endif
-    startTime = c;                                        // Save start time
-    while(((c = _getCycleCount()) - startTime) < t);      // Wait high duration
+    startTime = c; // Save start time
+    while (((c = _getCycleCount()) - startTime) < t)
+      ; // Wait high duration
 #ifdef ESP8266
-    GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, pinMask);       // Set low
+    GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, pinMask); // Set low
 #else
     gpio_set_level(pin, LOW);
 #endif
-    if(!(mask >>= 1)) {                                   // Next bit/byte
-      if(p >= end) break;
-      pix  = *p++;
+    if (!(mask >>= 1)) { // Next bit/byte
+      if (p >= end)
+        break;
+      pix = *p++;
       mask = 0x80;
     }
   }
-  while((_getCycleCount() - startTime) < period); // Wait for last bit
+  while ((_getCycleCount() - startTime) < period)
+    ; // Wait for last bit
 }
 
 #endif // ESP8266
--- examples/StrandtestBLE/BLESerial.cpp
+++ examples/StrandtestBLE/BLESerial.cpp
@@ -2,11 +2,10 @@
 
 // #define BLE_SERIAL_DEBUG
 
-BLESerial* BLESerial::_instance = NULL;
+BLESerial *BLESerial::_instance = NULL;
 
-BLESerial::BLESerial(unsigned char req, unsigned char rdy, unsigned char rst) :
-  BLEPeripheral(req, rdy, rst)
-{
+BLESerial::BLESerial(unsigned char req, unsigned char rdy, unsigned char rst)
+    : BLEPeripheral(req, rdy, rst) {
   this->_txCount = 0;
   this->_rxHead = this->_rxTail = 0;
   this->_flushed = 0;
@@ -24,9 +23,9 @@ BLESerial::BLESerial(unsigned char req, unsigned char rdy, unsigned char rst) :
 
 void BLESerial::begin(...) {
   BLEPeripheral::begin();
-  #ifdef BLE_SERIAL_DEBUG
-    Serial.println(F("BLESerial::begin()"));
-  #endif
+#ifdef BLE_SERIAL_DEBUG
+  Serial.println(F("BLESerial::begin()"));
+#endif
 }
 
 void BLESerial::poll() {
@@ -46,88 +45,97 @@ void BLESerial::end() {
 
 int BLESerial::available(void) {
   BLEPeripheral::poll();
-  int retval = (this->_rxHead - this->_rxTail + sizeof(this->_rxBuffer)) % sizeof(this->_rxBuffer);
-  #ifdef BLE_SERIAL_DEBUG
-    Serial.print(F("BLESerial::available() = "));
-    Serial.println(retval);
-  #endif
+  int retval = (this->_rxHead - this->_rxTail + sizeof(this->_rxBuffer)) %
+               sizeof(this->_rxBuffer);
+#ifdef BLE_SERIAL_DEBUG
+  Serial.print(F("BLESerial::available() = "));
+  Serial.println(retval);
+#endif
   return retval;
 }
 
 int BLESerial::peek(void) {
   BLEPeripheral::poll();
-  if (this->_rxTail == this->_rxHead) return -1;
+  if (this->_rxTail == this->_rxHead)
+    return -1;
   uint8_t byte = this->_rxBuffer[this->_rxTail];
-  #ifdef BLE_SERIAL_DEBUG
-    Serial.print(F("BLESerial::peek() = "));
-    Serial.print((char) byte);
-    Serial.print(F(" 0x"));
-    Serial.println(byte, HEX);
-  #endif
+#ifdef BLE_SERIAL_DEBUG
+  Serial.print(F("BLESerial::peek() = "));
+  Serial.print((char)byte);
+  Serial.print(F(" 0x"));
+  Serial.println(byte, HEX);
+#endif
   return byte;
 }
 
 int BLESerial::read(void) {
   BLEPeripheral::poll();
-  if (this->_rxTail == this->_rxHead) return -1;
+  if (this->_rxTail == this->_rxHead)
+    return -1;
   this->_rxTail = (this->_rxTail + 1) % sizeof(this->_rxBuffer);
   uint8_t byte = this->_rxBuffer[this->_rxTail];
-  #ifdef BLE_SERIAL_DEBUG
-    Serial.print(F("BLESerial::read() = "));
-    Serial.print((char) byte);
-    Serial.print(F(" 0x"));
-    Serial.println(byte, HEX);
-  #endif
+#ifdef BLE_SERIAL_DEBUG
+  Serial.print(F("BLESerial::read() = "));
+  Serial.print((char)byte);
+  Serial.print(F(" 0x"));
+  Serial.println(byte, HEX);
+#endif
   return byte;
 }
 
 void BLESerial::flush(void) {
-  if (this->_txCount == 0) return;
+  if (this->_txCount == 0)
+    return;
   this->_txCharacteristic.setValue(this->_txBuffer, this->_txCount);
   this->_flushed = millis();
   this->_txCount = 0;
   BLEPeripheral::poll();
-  #ifdef BLE_SERIAL_DEBUG
-    Serial.println(F("BLESerial::flush()"));
-  #endif
+#ifdef BLE_SERIAL_DEBUG
+  Serial.println(F("BLESerial::flush()"));
+#endif
 }
 
 size_t BLESerial::write(uint8_t byte) {
   BLEPeripheral::poll();
-  if (this->_txCharacteristic.subscribed() == false) return 0;
+  if (this->_txCharacteristic.subscribed() == false)
+    return 0;
   this->_txBuffer[this->_txCount++] = byte;
-  if (this->_txCount == sizeof(this->_txBuffer)) flush();
-  #ifdef BLE_SERIAL_DEBUG
-    Serial.print(F("BLESerial::write("));
-    Serial.print((char) byte);
-    Serial.print(F(" 0x"));
-    Serial.print(byte, HEX);
-    Serial.println(F(") = 1"));
-  #endif
+  if (this->_txCount == sizeof(this->_txBuffer))
+    flush();
+#ifdef BLE_SERIAL_DEBUG
+  Serial.print(F("BLESerial::write("));
+  Serial.print((char)byte);
+  Serial.print(F(" 0x"));
+  Serial.print(byte, HEX);
+  Serial.println(F(") = 1"));
+#endif
   return 1;
 }
 
 BLESerial::operator bool() {
   bool retval = BLEPeripheral::connected();
-  #ifdef BLE_SERIAL_DEBUG
-    Serial.print(F("BLESerial::operator bool() = "));
-    Serial.println(retval);
-  #endif
+#ifdef BLE_SERIAL_DEBUG
+  Serial.print(F("BLESerial::operator bool() = "));
+  Serial.println(retval);
+#endif
   return retval;
 }
 
-void BLESerial::_received(const uint8_t* data, size_t size) {
+void BLESerial::_received(const uint8_t *data, size_t size) {
   for (int i = 0; i < size; i++) {
     this->_rxHead = (this->_rxHead + 1) % sizeof(this->_rxBuffer);
     this->_rxBuffer[this->_rxHead] = data[i];
   }
-  #ifdef BLE_SERIAL_DEBUG
-    Serial.print(F("BLESerial::received("));
-    for (int i = 0; i < size; i++) Serial.print((char) data[i]);
-    Serial.println(F(")"));
-  #endif
+#ifdef BLE_SERIAL_DEBUG
+  Serial.print(F("BLESerial::received("));
+  for (int i = 0; i < size; i++)
+    Serial.print((char)data[i]);
+  Serial.println(F(")"));
+#endif
 }
 
-void BLESerial::_received(BLECentral& /*central*/, BLECharacteristic& rxCharacteristic) {
-  BLESerial::_instance->_received(rxCharacteristic.value(), rxCharacteristic.valueLength());
+void BLESerial::_received(BLECentral & /*central*/,
+                          BLECharacteristic &rxCharacteristic) {
+  BLESerial::_instance->_received(rxCharacteristic.value(),
+                                  rxCharacteristic.valueLength());
 }
