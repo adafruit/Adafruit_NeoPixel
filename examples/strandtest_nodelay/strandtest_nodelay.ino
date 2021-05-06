ff9bd8d19a50dc9f2aaa54a449e9d0c34558c3d8
Scotty Franzyshen <sfranzyshen@gmail.com>
2021-05-05 17:53:13 -0700

    Update strandtest_nodelay.ino
    First Attempt ...

--- examples/strandtest_nodelay/strandtest_nodelay.ino
+++ examples/strandtest_nodelay/strandtest_nodelay.ino
@@ -133,81 +133,50 @@ void theaterChase(uint32_t color, int wait) {
 }
 
 // Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
-void rainbow(int wait) {
-  // Hue of first pixel runs 5 complete loops through the color wheel.
-  // Color wheel has a range of 65536 but it's OK if we roll over, so
-  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
-  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
-  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
-    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-      // Offset pixel hue by an amount to make one full revolution of the
-      // color wheel (range of 65536) along the length of the strip
-      // (strip.numPixels() steps):
-      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
-      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
-      // optionally add saturation and value (brightness) (each 0 to 255).
-      // Here we're using just the single-argument hue variant. The result
-      // is passed through strip.gamma32() to provide 'truer' colors
-      // before assigning to each pixel:
-      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
-    }
-    strip.show(); // Update strip with new contents
-    delay(wait);  // Pause for a moment
+void rainbow(uint8_t wait) {
+  if(pixelInterval != wait)
+    pixelInterval = wait;                   //  Update delay time  
+  for(uint16_t i = 0; i < pixelNumber; i++;) {
+    strip.setPixelColor(i, Wheel((i + pixelCycle) & 255));
   }
+  strip.show();
+  pixelCycle++;
+  if(pixelCycle >= 256)
+    pixelCycle = 0;
 }
 
-// rainbow()
-void rnbw() {
-  
-  for(uint16_t i = 0; i < pixNum i++;) {
-    int p = (i + pixCycl) & 255;
-    pixs.setPixelColor(i, WheelR(p), WheelG(p), WheelB(p));
-    
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  if(pixelInterval != wait)
+    pixelInterval = wait;                   //  Update delay time  
+  for(int i = 0; i < pixelNumber; i + 3;) {
+    strip.setPixelColor(i + pixelQueue, Wheel((i + pixelCycle) % 255));
   }
-  pixs.show();
-  pixCycl++;
-  if(pixCycl >= 256) pixCycl = 0;
-}
-
-// Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
-void theaterChaseRainbow(int wait) {
-  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
-  for(int a=0; a<30; a++) {  // Repeat 30 times...
-    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
-      strip.clear();         //   Set all pixels in RAM to 0 (off)
-      // 'c' counts up from 'b' to end of strip in increments of 3...
-      for(int c=b; c<strip.numPixels(); c += 3) {
-        // hue of pixel 'c' is offset by an amount to make one full
-        // revolution of the color wheel (range 65536) along the length
-        // of the strip (strip.numPixels() steps):
-        int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
-        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
-        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
-      }
-      strip.show();                // Update strip with new contents
-      delay(wait);                 // Pause for a moment
-      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
-    }
-  }
-}
-
-// theaterChaseRainbow()
-void thtrChsRnbw() {
-  int i = 0;
-  int p = 0;
-  while(i < pixNum) {
-    p = (i + pixCycl) % 255;
-    pixs.setPixelColor(i + pixQueu, WheelR(p), WheelG(p), WheelB(p));
-    i = i + 3;
-  }
-  pixs.show();
+  strip.show();
   i = 0;
-  while( i < pixNum) {
-    pixs.setPixelColor(i + pixQueu, 0, 0, 0);
-    i = i + 3;
+  for( int i = 0; i < pixelNumber; i + 3;) {
+    strip.setPixelColor(i + pixelQueue, strip.Color(0, 0, 0));
   }      
-  pixQueu++;
-  pixCycl++;
-  if(pixQueu >= 3) pixQueu = 0;
-  if(pixCycl >= 256) pixCycl = 0;
+  pixelQueue++;
+  pixelCycle++;
+  if(pixelQueue >= 3)
+    pixelQueue = 0;
+  if(pixelCycle >= 256)
+    pixelCycle = 0;
 }
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
