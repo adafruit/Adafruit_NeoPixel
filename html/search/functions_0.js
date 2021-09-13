db3c894f019414b9dc3313f5aa30b2acf8e97ff8
Doxygen CI <ci-arduino@invalid>
2021-09-13 20:25:54 +0000

    Deploy docs to GitHub Pages from commit 9a05471ebe
    Commit: 9a05471ebef95e53b1ba72f8c057d493b13c043c
    GitHub Actions run: 1231088831

--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['adafruit_5fneopixel',['Adafruit_NeoPixel',['../class_adafruit___neo_pixel.html#a163433471a799ba29f941032601b4a9d',1,'Adafruit_NeoPixel::Adafruit_NeoPixel(uint16_t n, uint16_t pin=6, neoPixelType type=NEO_GRB+NEO_KHZ800)'],['../class_adafruit___neo_pixel.html#a2a7ab8a0cedee029cd5330b638146ccc',1,'Adafruit_NeoPixel::Adafruit_NeoPixel(void)']]]
+  ['adafruit_5fneopixel',['Adafruit_NeoPixel',['../class_adafruit___neo_pixel.html#aac61dab69181d4554be8cdde40e4bce6',1,'Adafruit_NeoPixel::Adafruit_NeoPixel(uint16_t n, int16_t pin=6, neoPixelType type=NEO_GRB+NEO_KHZ800)'],['../class_adafruit___neo_pixel.html#a2a7ab8a0cedee029cd5330b638146ccc',1,'Adafruit_NeoPixel::Adafruit_NeoPixel(void)']]]
 ];
