5561fc21b7614c3f66e1b6847816b0973f5c5e3f
Doxygen CI <ci-arduino@invalid>
2023-02-07 17:46:13 +0000

    Deploy docs to GitHub Pages from commit 9a9104c6ed
    Commit: 9a9104c6ede08097f8067f4005837ff56a3d2936
    GitHub Actions run: 4116675962

--- html/search/functions_7.js
+++ html/search/functions_7.js
@@ -4,5 +4,6 @@ var searchData=
   ['setpin',['setPin',['../class_adafruit___neo_pixel.html#aa1898f089b9a4e02841cec2e63b88c33',1,'Adafruit_NeoPixel']]],
   ['setpixelcolor',['setPixelColor',['../class_adafruit___neo_pixel.html#ab8763ccc6f9a090df1f753905fd5561e',1,'Adafruit_NeoPixel::setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b)'],['../class_adafruit___neo_pixel.html#ac7b3529b3d190f3af5d8e5b9ead27688',1,'Adafruit_NeoPixel::setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b, uint8_t w)'],['../class_adafruit___neo_pixel.html#a19fc274330c0e65907929ee03b93b1c3',1,'Adafruit_NeoPixel::setPixelColor(uint16_t n, uint32_t c)']]],
   ['show',['show',['../class_adafruit___neo_pixel.html#a4bcda7a15591065570d3598cbf970be7',1,'Adafruit_NeoPixel']]],
-  ['sine8',['sine8',['../class_adafruit___neo_pixel.html#afb00024000da81bcb6378c7052f77be3',1,'Adafruit_NeoPixel']]]
+  ['sine8',['sine8',['../class_adafruit___neo_pixel.html#afb00024000da81bcb6378c7052f77be3',1,'Adafruit_NeoPixel']]],
+  ['str2order',['str2order',['../class_adafruit___neo_pixel.html#a5cec6fe17e75f8d607df305e2fa9db5c',1,'Adafruit_NeoPixel']]]
 ];
