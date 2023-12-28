a2a2486dee6d1244786bad4b08ebf370ee15de84
himanshudiwane <108461333+himanshudiwane@users.noreply.github.com>
2023-12-28 10:46:33 +0530

    Added support for VEGA ARIES Boards
    The VEGA ARIES boards is a fully indigenous and a “Made in India” product to get started with basic microprocessor programming and embedded systems. This board is built upon a RISC-V ISA compliant VEGA Processor with easy-to-use hardware and software. The board is added in Arduino IDE.

--- Adafruit_NeoPixel.cpp
+++ Adafruit_NeoPixel.cpp
@@ -3026,6 +3026,64 @@ if(is800KHz) {
     }
   }
 
+#elif defined(VEGA_ARIES_V2) || defined(VEGA_ARIES_V3) || defined(VEGA_ARIES_IOT) || defined(VEGA_ARIES_MICRO)
+
+#define CYCLES_800_T0H (F_CPU / 2500000) // 0.4us
+#define CYCLES_800_T1H (F_CPU / 1250000) // 0.8us
+#define CYCLES_800 (F_CPU / 800000)      // 1.25us per bit
+#define CYCLES_400_T0H (F_CPU / 2000000) // 0.5uS
+#define CYCLES_400_T1H (F_CPU / 833333)  // 1.2us
+#define CYCLES_400 (F_CPU / 400000)      // 2.5us per bit
+
+    uint8_t *p, *end, pix, mask;
+    uint32_t t, time0, time1, period, c, startTime;
+
+    p = pixels;
+    end = p + numBytes;
+    pix = *p++;
+    mask = 0x80;
+    startTime = 0;
+
+#ifdef NEO_KHZ400
+    if (is800KHz)
+    {
+#endif
+        time0 = CYCLES_800_T0H;
+        time1 = CYCLES_800_T1H;
+        period = CYCLES_800;
+#ifdef NEO_KHZ400
+    }
+    else
+    { // 400 KHz bitstream
+        time0 = CYCLES_400_T0H;
+        time1 = CYCLES_400_T1H;
+        period = CYCLES_400;
+    }
+#endif
+
+    for (t = time0;; t = time0)
+    {
+        if (pix & mask)
+            t = time1; // Bit high duration
+        while (((c = read_csr(mcycle)) - startTime) < period)
+            ; // Wait for bit start
+        digitalWrite(pin, HIGH);
+        startTime = c; // Save start time
+        while (((c = read_csr(mcycle)) - startTime) < t)
+            ; // Wait high duration
+        digitalWrite(pin, LOW);
+
+        if (!(mask >>= 1))
+        { // Next bit/byte
+            if (p >= end)
+                break;
+            pix = *p++;
+            mask = 0x80;
+        }
+    }
+    while ((read_csr(mcycle) - startTime) < period)
+        ; // Wait for last bit
+	
 #else
 #error Architecture not supported
 #endif
