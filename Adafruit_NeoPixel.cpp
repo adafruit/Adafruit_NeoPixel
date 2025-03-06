d1eebc2d6935f28cde4e665482bd45345b3468c9
brentru <brent@adafruit.com>
2025-03-06 16:01:11 -0500

    call indirectly to espshow in dtor

--- Adafruit_NeoPixel.cpp
+++ Adafruit_NeoPixel.cpp
@@ -122,11 +122,14 @@ Adafruit_NeoPixel::Adafruit_NeoPixel()
   @brief   Deallocate Adafruit_NeoPixel object, set data pin back to INPUT.
 */
 Adafruit_NeoPixel::~Adafruit_NeoPixel() {
+#ifdef ARDUINO_ARCH_ESP32
+  // Release RMT resources (RMT channels and led_data)
+  // by indirectly calling into espShow()
+  memset(pixels, 0, numBytes);
+  numLEDs = numBytes = 0;
+  show();
+#endif
   free(pixels);
-  #ifdef ARDUINO_ARCH_ESP32
-  // Release RMT resources (RMT channel and led_data)
-  espShow(pin, NULL, 0, true);
-  #endif
   if (pin >= 0)
     pinMode(pin, INPUT);
 }
